d3de617eb7e6edc2ca5fd817a89995944fcfd2a4
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-06-03 13:26:43 +0200

    Add a default config file.

---
+++ include/config.pm
@@ -0,0 +1,15 @@
+package Tim::Config;
+
+use 5.020;
+use strict;
+use warnings;
+
+our @channels = ("#956f7fd1ae68f");
+our $server = "irc.freenode.net";
+our $server_port = 6667;
+
+our $nick = "tim_the_bot";
+our $username = $nick;
+our $real_name = "kek";
+
+our $reconnect_wait_sec = 60;

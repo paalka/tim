43da3fe5ca851b67e58a3289373a8e731dc013ad
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-06-19 18:14:47 +0200

    Add a basic module for obtaining information about bus routes.

---
+++ include/Modules/AtB.pm
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use 5.020;
+use strict;
+use warnings;
+use utf8;
+
+package Modules::AtB;
+
+use JSON::MaybeXS;
+use XML::Simple qw(:strict);
+
+use lib '../';
+use Tim;
+
+sub ask_bus_oracle {
+    my $question = shift;
+
+    return 'Invalid question! Usage: !<command> $question.' unless (defined($question));
+
+    my $base_url = 'https://www.atb.no/xmlhttprequest.php?service=routeplannerOracle.getOracleAnswer&question=';
+    my $url = $base_url . $question;
+
+    my $oracle_response = Tim::trim_whitespace(Tim::make_request($url));
+    return $oracle_response;
+}
+
+1;
